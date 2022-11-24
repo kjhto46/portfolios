f42460be3088c6bf76ed62be8ff544540c9bd6f3
권준희 <81128476+kjhto46@users.noreply.github.com>
2022-11-24 23:04:29 +0900

    ajax로 불러오기

---
+++ test01/ajax.js
@@ -0,0 +1,86 @@
+export default [
+  {
+    img: "01.jpg",
+    subject: "제목입니다",
+    subtxt: "내용입니다",
+    link: "www.naver.come",
+  },
+  {
+    img: "01.jpg",
+    subject: "제목입니다",
+    subtxt: "내용입니다",
+    link: "www.naver.come",
+  },
+  {
+    img: "01.jpg",
+    subject: "제목입니다",
+    subtxt: "내용입니다",
+    link: "www.naver.come",
+  },
+  {
+    img: "01.jpg",
+    subject: "제목입니다",
+    subtxt: "내용입니다",
+    link: "www.naver.come",
+  },
+  {
+    img: "01.jpg",
+    subject: "제목입니다",
+    subtxt: "내용입니다",
+    link: "www.naver.come",
+  },
+  {
+    img: "01.jpg",
+    subject: "제목입니다",
+    subtxt: "내용입니다",
+    link: "www.naver.come",
+  },
+  {
+    img: "01.jpg",
+    subject: "제목입니다",
+    subtxt: "내용입니다",
+    link: "www.naver.come",
+  },
+  {
+    img: "01.jpg",
+    subject: "제목입니다",
+    subtxt: "내용입니다",
+    link: "www.naver.come",
+  },
+  {
+    img: "01.jpg",
+    subject: "제목입니다",
+    subtxt: "내용입니다",
+    link: "www.naver.come",
+  },
+  {
+    img: "01.jpg",
+    subject: "제목입니다",
+    subtxt: "내용입니다",
+    link: "www.naver.come",
+  },
+  {
+    img: "01.jpg",
+    subject: "제목입니다",
+    subtxt: "내용입니다",
+    link: "www.naver.come",
+  },
+  {
+    img: "01.jpg",
+    subject: "제목입니다",
+    subtxt: "내용입니다",
+    link: "www.naver.come",
+  },
+  {
+    img: "01.jpg",
+    subject: "제목입니다",
+    subtxt: "내용입니다",
+    link: "www.naver.come",
+  },
+  {
+    img: "01.jpg",
+    subject: "제목입니다",
+    subtxt: "내용입니다",
+    link: "www.naver.come",
+  },
+];
